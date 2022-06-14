9e88b711b13ba78901c04ff9eac176ac40f3f2ed
Programmist <66207711+Programmist21rus@users.noreply.github.com>
2022-06-14 23:56:22 +0300

    Вопрос

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,28 @@ window.onload = function () {
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
 }
+
+$(document).ready(function () {
+
+    $(window).scroll(() => {
+        let scrollDistance = $(window).scrollTop();
+
+        $('.section').each((i, el) => {
+            if ($(el).offset().top() - $('nav').outerHeight() <= scrollDistance) {
+                $('nav a').each((i, el) => {
+                    if ($(el).hasClass('active')) {
+                        $(el).removeClass('active');
+                    }
+                });
+                $('nav li:eq(' + i + ')').find('a').addClass('active');
+            }
+        });
+
+    });
+
+});
+
+$("a[href^='#']").click(function () {
+    let valHref = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(valHref).offset().top() - 50 + "px"});
+});
