fc395cb7c5a7651b97fec0d2c8eaebbe51afeeb3
Programmist <66207711+Programmist21rus@users.noreply.github.com>
2022-06-16 11:46:23 +0300

    end

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         let scrollDistance = $(window).scrollTop();
 
         $('.section').each((i, el) => {
-            if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
+            if ($(el).offset().top - ($('nav').outerHeight() * 3) <= scrollDistance) {
                 $('nav a').each((i, el) => {
                     if ($(el).hasClass('active')) {
                         $(el).removeClass('active');
@@ -27,3 +27,63 @@ $("a[href^='#']").click(function () {
     let valHref = $(this).attr("href");
     $("html, body").animate({scrollTop: $(valHref).offset().top - 50 + "px"});
 });
+
+function calculate() {
+    let day = 0;
+    let cost = 0;
+    let select = document.getElementById('list1');
+    let select1 = document.getElementById('list2');
+    let select2 = document.getElementById('list3');
+    if(select.value === "Opt1") {
+        day += 1;
+        cost += 1000;
+    }
+
+    else if(select.value === "Opt2") {
+        day += 2;
+        cost += 2000;
+    }
+
+    else {
+        day += 3;
+        cost += 3000;
+    }
+
+    if(select1.value === "Opt1") {
+        day += 1;
+        cost += 1000;
+    }
+
+    else if(select1.value === "Opt2") {
+        day += 2;
+        cost += 2000;
+    }
+
+    else {
+        day += 3;
+        cost += 3000;
+    }
+
+    if(select2.value === "Opt1") {
+        day += 1;
+        cost += 1000;
+    }
+
+    else if(select2.value === "Opt2") {
+        day += 2;
+        cost += 2000;
+    }
+
+    else {
+        day += 3;
+        cost += 3000;
+    }
+
+    if(!select.value || !select1.value || !select2.value) {
+        alert("Введите все пункты!!!");
+    }
+    else {
+        document.getElementById("day").innerHTML = day;
+        document.getElementById("cost").innerHTML = cost;
+    }
+}
